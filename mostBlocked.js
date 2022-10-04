bdd5ad96685219f8c981e2b6336c5b1cc00865c9
Adriano Harm <adhkr81@gmail.com>
2022-10-04 16:09:45 -0300

    loops refactored

--- mostBlocked.js
+++ mostBlocked.js
@@ -3,14 +3,14 @@ fs = require('fs')
 
 function mostBlocked (data) {
     
-    // const statesCount = {}
-    const statesCountTest = []
 
+    const statesCount = {}
+    // const statesCountOld = {}
 
     // for (let i = 0; i < data.length; i++) {
     //     if (data[i].subscription.status === "Blocked") {
-    //         statesCount[data[i].address.state] = statesCount[data[i].address.state]
-    //                     ? statesCount[data[i].address.state] += 1
+    //         statesCountOld[data[i].address.state] = statesCountOld[data[i].address.state]
+    //                     ? statesCountOld[data[i].address.state] += 1
     //                     : 1                                  
     //        } 
     // }
@@ -18,50 +18,59 @@ function mostBlocked (data) {
 
     for (let i = 0; i < data.length; i++) {
         if (data[i].subscription.status === "Blocked") {
-            
-            if (statesCountTest[data[i].address.state] === undefined) {
-                statesCountTest.push({[data[i].address.state] : {count: 1, users: {"first_name": data[i].first_name,
-                                                                                "last_name": data[i].last_name,
-                                                                                "address" : data[i].address}} })  
-            }
-       } 
-    }
 
-    
-
-    console.log(statesCountTest)
-
-
-    const keys = Object.keys(statesCount)
-    let count = 0
-    mostBlockedStates = []
-
-    for (let j = 0; j < keys.length; j++) {
-
-        if(statesCount[keys[j]] > count) {
-            mostBlockedStates = [keys[j]]
-            count = statesCount[keys[j]] 
-        } else if (statesCount[keys[j]] === count) {
-            mostBlockedStates.push(keys[j])
-        } 
-    }
-
-
-    console.log(count)
-    console.log(mostBlockedStates)
-
-
-
-    let blockedUsers = []
-    data.map((current) => {
-        if (mostBlockedStates.indexOf(current.address.state) != -1) {
-            return blockedUsers.push(
-                {"first_name": current.first_name,
-                "last_name": current.last_name,
-                "address" : current.address}
-            )
+          if (statesCount[data[i].address.state] === undefined) {
+            statesCount[data[i].address.state] = {
+              count: 1,
+              users: {
+                      first_name: data[i].first_name,
+                      last_name: data[i].last_name,
+                      address: data[i].address,
+                    }
+            } 
+          } else {
+            statesCount[data[i].address.state] = {
+                count: statesCount[data[i].address.state].count + 1,
+                users: {...statesCount[data[i].address.state].users, ...{
+                    first_name: data[i].first_name,
+                    last_name: data[i].last_name,
+                    address: data[i].address,
+                  }}                 
+              } 
+          }
         }
-    })
+      }
+
+
+
+    console.log(statesCount)
+
+
+    // const keys = Object.keys(statesCount)
+    // let count = 0
+    // mostBlockedStates = []
+
+    // for (let j = 0; j < keys.length; j++) {
+
+    //     if(statesCount[keys[j]] > count) {
+    //         mostBlockedStates = [keys[j]]
+    //         count = statesCount[keys[j]] 
+    //     } else if (statesCount[keys[j]] === count) {
+    //         mostBlockedStates.push(keys[j])
+    //     } 
+    // }
+
+
+    // let blockedUsers = []
+    // data.map((current) => {
+    //     if (mostBlockedStates.indexOf(current.address.state) != -1) {
+    //         return blockedUsers.push(
+    //             {"first_name": current.first_name,
+    //             "last_name": current.last_name,
+    //             "address" : current.address}
+    //         )
+    //     }
+    // })
 
     // console.log(blockedUsers)
 }
